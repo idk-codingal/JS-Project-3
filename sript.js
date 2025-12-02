ac4b08a1b17f04874eb1d30d8e59dd38e68cce34
idk-codingal <mokshnagda2020@gmail.com>
2025-12-01 16:58:52 -0800

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,13 @@
+var str = "Goku ( aka: Kakkarot) is the strongest anime character"
+document.write(str.toUpperCase());
+document.write("<br>")
+document.write("<br>")
+
+var str = "Vegeta is Goku's best friend"
+document.write(str.toLowerCase());
+document.write("<br>")
+document.write("<br>")
+
+
+document.write(str.startsWith("V"));
+document.write("<br>")
